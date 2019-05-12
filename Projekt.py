9439220fe30baec33c42e80104fc515199e2e123
TSiedlecki <45299682+TSiedlecki@users.noreply.github.com>
2019-05-12 11:21:23 +0200

    Add files via upload

--- Projekt.py
+++ Projekt.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -15,7 +14,7 @@ class Projekt:
 
     def dataset_generator(self):
         self.adult_train = pd.read_csv("Adult_train.tab", '\t')
-        self.adult_test = pd.read_csv("Adult_test_without_class.tab", '\t')
+        self.adult_test = pd.read_csv("Adults_test_without_class.tab", '\t')
 
 
     def dataset_preprocesin(self):
@@ -36,12 +35,24 @@ class Projekt:
         y_data = self.adult_train.loc[:, ["y_cat"]]
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x_data, y_data, test_size=0.2)
 
+        self.adult_test["workclass_cat"] = pd.Categorical(self.adult_test.workclass).codes
+        self.adult_test["education_cat"] = pd.Categorical(self.adult_test.education).codes
+        self.adult_test["marital_status_cat"] = pd.Categorical(self.adult_test.marital_status).codes
+        self.adult_test["occupation_cat"] = pd.Categorical(self.adult_test.occupation).codes
+        self.adult_test["relationship_cat"] = pd.Categorical(self.adult_test.relationship).codes
+        self.adult_test["race_cat"] = pd.Categorical(self.adult_test.race).codes
+        self.adult_test["sex_cat"] = pd.Categorical(self.adult_test.sex).codes
+        self.adult_test["native_country_cat"] = pd.Categorical(self.adult_test.native_country).codes
+        self.x_data_test = self.adult_test.loc[:, columns]
+
+
+
     def training_process(self):
         methods = [RandomForestClassifier(n_estimators=100, min_samples_split=5), DecisionTreeClassifier(), KNeighborsClassifier(n_neighbors=3)]
 
         res_train = []
         res_train_f = []
-        res_test_f = []
+        self.res_test_f = []
         res_test = []
 
         j = 1
@@ -51,7 +62,7 @@ class Projekt:
 
             rf_clas = method.fit(self.x_train, self.y_train)
             y_pred_train = rf_clas.predict(self.x_train)
-            y_pred_test = rf_clas.predict(self.adult_test)
+            y_pred_test = rf_clas.predict(self.x_data_test)
 
 
             # met.accuracy_score(self.y_train, y_pred_train)
@@ -78,11 +89,22 @@ class Projekt:
 
 
             if kk == 1:
-                res_test_f = y_pred_test
+                self.res_test_f = y_pred_test
             else:
                 for index, v in enumerate(res_test):
-                    res_test_f[index] = res_test_f[index] + y_pred_test[index]
+                    self.res_test_f[index] = res_test_f[index] + y_pred_test[index]
             kk = kk+1
+
+    def output_data(self):
+        print(len(self.res_test_f))
+        xzy="["
+        for item in self.res_test_f:
+            xzy+=str(item)+","
+        xzy+="]"
+        with open('result.txt', 'w') as f:
+
+                f.write(xzy)
+
 
 
 
@@ -90,3 +112,4 @@ projekt = Projekt()
 projekt.dataset_generator()
 projekt.dataset_preprocesin()
 projekt.training_process()
+projekt.output_data()
